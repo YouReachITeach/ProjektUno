981d6b7b4d31bd6b6b3c2dece477f73a5e2c248c
Levin <147081970+YouReachITeach@users.noreply.github.com>
2025-04-14 17:00:27 +0200

    Initial commit

---
+++ src/main/java/org/example/projektuno/ProjektUnoApplication.java
@@ -0,0 +1,13 @@
+package org.example.projektuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektUnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektUnoApplication.class, args);
+    }
+
+}
