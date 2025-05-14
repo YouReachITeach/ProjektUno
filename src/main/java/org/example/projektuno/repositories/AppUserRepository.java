fd099aaf6abb7b714413aa29fa78f0e663e79f7c
Levin <147081970+YouReachITeach@users.noreply.github.com>
2025-05-14 23:30:58 +0200

    @Repository annotation

--- src/main/java/org/example/projektuno/repositories/AppUserRepository.java
+++ src/main/java/org/example/projektuno/repositories/AppUserRepository.java
@@ -2,9 +2,11 @@ package org.example.projektuno.repositories;
 
 import org.example.projektuno.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
 }
--- src/main/java/org/example/projektuno/repositories/UserTeamRepository.java
+++ src/main/java/org/example/projektuno/repositories/UserTeamRepository.java
@@ -2,9 +2,11 @@ package org.example.projektuno.repositories;
 
 import org.example.projektuno.entity.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
     Optional<UserTeam> findByUserId(Long userId);
 }
