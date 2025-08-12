0d342b504cb01cc66dcd4002b55704c803c4d7a0
Levin <147081970+YouReachITeach@users.noreply.github.com>
2025-08-12 20:02:40 +0100

    Test entfernt aus application fürs erste

--- src/main/java/org/example/projektuno/ProjektUnoApplication.java
+++ src/main/java/org/example/projektuno/ProjektUnoApplication.java
@@ -1,7 +1,5 @@
 package org.example.projektuno;
 
-import org.example.projektuno.service.TestDataService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -10,8 +8,6 @@ import org.springframework.context.event.EventListener;
 @SpringBootApplication
 public class ProjektUnoApplication {
 
-    @Autowired
-    private TestDataService testDataService;
 
     public static void main(String[] args) {
         SpringApplication.run(ProjektUnoApplication.class, args);
